196456aaf0478cb06a01f1eec7fdba5bd49527b9
Pollux.Cong <pollux.cong@matrixport.com>
2024-01-05 17:47:28 +0800

    resolve the overloaded member functions

--- TestReferenceData.cpp
+++ TestReferenceData.cpp
@@ -8,8 +8,9 @@ PYBIND11_MODULE(sbe, m) {
     m.doc() = "test using pybind11 for reference data"; // optional module docstring
 
     py::class_<sbe::Header>(m,"Header")
-        .def("wrap",&sbe::Header::wrap);
-        .def("fragment",&sbe::Header::fragment);
+        .def("wrap",&sbe::Header::wrap)
+        //.def("fragment",static_cast<std::uint8_t (sbe::Header::*)() const>(&sbe::Header::fragment)); // this works for c++<=14
+        .def("fragment",py::overload_cast<>(&sbe::Header::fragment,py::const_));
 
     py::class_<sbe::ReferenceData>(m,"ReferenceData")
         .def("wrap",&sbe::ReferenceData::wrapForDecode);
